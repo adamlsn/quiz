2876cc53134f7431a61d2fd90650df2dfd2b5089
Adam Olson <adamlsn@gmail.com>
2021-07-28 20:25:43 -0500

    fixing init(), adding basic questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,56 @@
 const startBtn = document.getElementById("start-btn");
 const questionEl = document.getElementById("questions-container");
 
+const questions = [
+    {
+        question: "QUESTION ONE",
+        answers: [
+            { text: "ONE", correct: false },
+            { text: "TWO", correct: false },
+            { text: "THREE", correct: true },
+            { text: "FOUR", correct: false }
+        ]
+    },
+    {   
+        question: "QUESTION TWO",
+        answers: [
+            { text: "ONE", correct: false },
+            { text: "TWO", correct: true },
+            { text: "THREE", correct: false },
+            { text: "FOUR", correct: false }
+        ]
+    },   
+    {   
+        question: "QUESTION THREE",
+        answers: [
+            { text: "ONE", correct: false },
+            { text: "TWO", correct: true },
+            { text: "THREE", correct: false },
+            { text: "FOUR", correct: false }
+        ]
+    },
+    {   
+        question: "QUESTION FOUR",
+        answers: [
+            { text: "ONE", correct: false },
+            { text: "TWO", correct: true },
+            { text: "THREE", correct: false },
+            { text: "FOUR", correct: false }
+        ]
+    },
+    {   
+        question: "QUESTION FIVE",
+        answers: [
+            { text: "ONE", correct: false },
+            { text: "TWO", correct: true },
+            { text: "THREE", correct: false },
+            { text: "FOUR", correct: false }
+        ]
+    }      
+]
+
 startBtn.addEventListener("click", init);
 
-init = () =>{
+function init(){
     console.log("BUTTON WORKS")
 }
